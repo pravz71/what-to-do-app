46f0497dfc0387295c2a12fb4f8140487b8297e1
Pravin Gupta <pravingupta1071@gmail.com>
2019-08-21 16:48:22 +0530

    removing unnecessary files

--- src/util.js
+++
@@ -1,7 +0,0 @@
-console.log('Util.js is running....');
-
-const square = x => x * x;
-const add = (a, b) => a + b;
-export default (a, b) => b - a;
-export { square };
-// export { square, add as default };
